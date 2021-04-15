d1e6b023104a9aff2d868be53ac782765d7f87d6
Hiro <hiroshi.takeuchi111@gmail.com>
2021-04-15 03:07:42 +0000

    added one test case in controller.test.ts

--- src/controllers/controller.test.ts
+++ src/controllers/controller.test.ts
@@ -3,16 +3,27 @@ import { getController } from "./controller";
 
 const ctlr = getController({} as any, {} as any);
 
+/** response mock object */
+const res = {
+  status: jest.fn().mockReturnThis(),
+  send: jest.fn(),
+} as any;
+
+/** request mock object */
+const req = {} as any;
+
+const statusMock = res.status as jest.Mock;
+const sendMock = res.send as jest.Mock;
+
 describe("getController", () => {
   describe("getRoot", () => {
     it("should return OK message", () => {
-      // const res = {
-      //   status: (statuCode: number) => res,
-      //   send: (body: any) => {},
-      // } as Response;
-      // ctlr.getRoot({} as any, res);
-      // expect(res.status).toHaveBeenCalledTimes(1);
-      // expect(res.send).toHaveBeenCalledTimes(1);
+      expect.assertions(4);
+      ctlr.getRoot(req, res);
+      expect(res.status).toHaveBeenCalledTimes(1);
+      expect(statusMock.mock.calls[0][0]).toEqual(200);
+      expect(res.send).toHaveBeenCalledTimes(1);
+      expect(sendMock.mock.calls[0][0]).toEqual({ message: "OK" });
     });
   });
 });
