210cc910bb0bd7c90a933ea4c8a3b2d402e4dc3b
Hiro <hiroshi.takeuchi111@gmail.com>
2021-08-23 01:38:21 +0000

    updated.github/workflows/*.yaml

--- client/tmp.js
+++
@@ -1,8 +0,0 @@
-const { v4: uuid } = require("uuid");
-
-const func = (id) => ({ id, time: Date.now() - id });
-
-const x = [func(1), func(1000), func(10000), func(50), func(120)];
-
-console.log(x);
-console.log(x.sort((a, b) => b.time - a.time));
---
+++ nginx/README.md
@@ -0,0 +1,3 @@
+# Nginx (Reverse Proxy)
+
+Config file under this direcotry is used only for reverse proxy container in development environment (see docker-compose.yml)
