5ecb1d42b2840283b50afd9486c765b7f74087a9
Hiro <hiroshi.takeuchi111@gmail.com>
2021-08-27 02:45:53 +0000

    udpated wsContoller.ts that it sends 'join a new room' event with channel data

--- api/src/chats/controllers/wsController.ts
+++ api/src/chats/controllers/wsController.ts
@@ -5,6 +5,7 @@ import { Queries } from "../queries/query";
 
 import { getCheckMember } from "./utils";
 import { validateMessage } from "../../utils/utils";
+import Channel from "../models/Channel.model";
 
 // This stores user ID as key, socket object as value
 const userSession: { [key: string]: Socket } = {};
@@ -18,9 +19,9 @@ export const sendExceptionToSender = (
 };
 
 // helper function that lets user join a room, then send a event to the user
-const joinAndNotifyUser = (channelId: string, socket: Socket): void => {
-  socket.join(channelId);
-  socket.emit("joined a new room", { channelId });
+const joinAndNotifyUser = (channel: Channel, socket: Socket): void => {
+  socket.join(channel.id);
+  socket.emit("joined a new room", { channel });
 };
 
 export interface WSController {
@@ -169,7 +170,6 @@ export const getWSController = (
     },
 
     onJoinNewRoom: async (io: Server, socket: Socket, data: any) => {
-      // console.log("onJoinNewRoom() invoked, data: ", data);
       // validate user
       const { userId, username } = socket.request.session;
       if (!username || !userId)
@@ -202,10 +202,20 @@ export const getWSController = (
             detail: `sender is not a member of channel ${channelId}`,
             timestamp: Date.now(),
           });
+        // get channel data from db
+        const channel = await channelQuery.getChannelById(channelId);
+        if (!channel)
+          return sendExceptionToSender(socket, {
+            code: 400, // bad request
+            detail: `channel not found: ${channelId}`,
+            timestamp: Date.now(),
+          });
         // let users join the room
-        (await userQuery.getUsersByChannelId(channelId)).forEach((user) => {
-          joinAndNotifyUser(channelId, userSession[user.id]);
-        });
+        (await userQuery.getUsersByChannelId(channelId))
+          .filter((user) => user.id !== userId)
+          .forEach((user) => {
+            joinAndNotifyUser(channel, userSession[user.id]);
+          });
       } catch (e) {
         return sendExceptionToSender(socket, {
           code: 500,
